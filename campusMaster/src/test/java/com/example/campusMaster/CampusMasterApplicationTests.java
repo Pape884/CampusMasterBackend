1e5c9a3f0d86adae52b129b4d42c3abf4b2072d9
Pape Sow <sowpape979@gmail.com>
2025-12-14 11:32:54 +0000

    initialisation

---
+++ campusMaster/src/test/java/com/example/campusMaster/CampusMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campusMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
